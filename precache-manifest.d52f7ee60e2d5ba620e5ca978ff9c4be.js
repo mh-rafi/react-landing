b7bba9833ffef8b0bb08eaf1f8deaaf8232c6fb5
mh-rafi <rafi.bogra@gmail.com>
2020-05-21 16:25:13 +0600

    Updates

---
+++ precache-manifest.d52f7ee60e2d5ba620e5ca978ff9c4be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2638cc8e3bad7b9374f2a1b3347ee38",
+    "url": "/react-landing/index.html"
+  },
+  {
+    "revision": "a2af213a984867e4e7b2",
+    "url": "/react-landing/static/css/2.9d5983be.chunk.css"
+  },
+  {
+    "revision": "609e8d91fe005be5b4a3",
+    "url": "/react-landing/static/css/main.ed1f2490.chunk.css"
+  },
+  {
+    "revision": "a2af213a984867e4e7b2",
+    "url": "/react-landing/static/js/2.2926a316.chunk.js"
+  },
+  {
+    "revision": "bfee40bc6f2ca1dab24894f0f4a013a9",
+    "url": "/react-landing/static/js/2.2926a316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609e8d91fe005be5b4a3",
+    "url": "/react-landing/static/js/main.4881408d.chunk.js"
+  },
+  {
+    "revision": "8a162885d18be97721f8",
+    "url": "/react-landing/static/js/runtime-main.163a9321.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e8f55c02c41646da495f971c5c059bf.js"
+  "/react-landing/precache-manifest.d52f7ee60e2d5ba620e5ca978ff9c4be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-landing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
